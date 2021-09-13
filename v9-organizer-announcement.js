7a524d513cdef083b3b801525c8e5c7bb071b2bc
Vic Chimenti <victor.chimenti@gmail.com>
2021-09-13 09:58:57 -0700

    udv

--- v9-organizer-announcement.js
+++ v9-organizer-announcement.js
@@ -8,7 +8,7 @@
    *
    *     Document will write once when the page loads
    *
-   *     @version 5.11
+   *     @version 5.12
    */
 
 
@@ -24,6 +24,8 @@
     importClass(com.terminalfour.media.utils.ImageInfo);
     importClass(com.terminalfour.media.MediaManager);
 
+
+    
 
     // Returns a media stream object
     function readMedia(mediaID) {
@@ -137,34 +139,6 @@
         openImageWrapper = '<div class="col-md-4">';
     }
 
-
-//     var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
-// var optionToTestFor = "suLawInTheNews"; //edit this to change the option
-// var contentTypeLayout = 'output/gridfeed'; //edit this to change the Content Layout to use for output
-// var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
-
-
-/***
- *  send correct layout to the document
- * 
- */
-try {
-
-    /* if content exists, it'll start at 0 or later, so process this */
-    if ((n >= 0)) {
-        var sw = new java.io.StringWriter();
-        var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
-        new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);
-        output = sw.toString();
-
-        // write to page document
-        document.write(output);
-    }
-
-
-} catch (err) {
-    document.write(err.message);
-}
 
 
 
