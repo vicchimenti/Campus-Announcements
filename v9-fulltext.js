c71d90a9b78c109692f361b5a446ec52964916af
Vic Chimenti <victor.chimenti@gmail.com>
2021-09-13 13:06:25 -0700

    rm author

--- v9-fulltext.js
+++ v9-fulltext.js
@@ -17,14 +17,14 @@ try {
     *  default declarations
     * 
     * */
-   var name = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='normal' modifiers='striptags,htmlentities' />");
+   var contentName = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='normal' modifiers='striptags,htmlentities' />");
    var articleTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Title' output='normal' display_field='value' />");
    var articleCaption = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Caption' output='normal' display_field='value' />");
-   var articleAuthor = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' display_field='value' />");
+//    var articleAuthor = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' display_field='value' />");
    var articlePhotoCredit = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Photography By' output='normal' display_field='value' />");
    var publishDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Publish Date' output='normal' date_format='MMMM d, yyyy' />");
    var articleImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Image' output='normal' formatter='path/*' />");
-   var articleImageAlt = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='media' name='Image' attribute='description' />");
+//    var articleImageAlt = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='media' name='Image' attribute='description' />");
    var externalLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link' output='normal' modifiers='striptags,htmlentities' />");
    var externalLinkText = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link Text' output='normal' modifiers='striptags,htmlentities' />");
    var articleFullBody = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Body' output='normal' display_field='value' />");
@@ -48,7 +48,7 @@ try {
    var openPublishWrapper = '<div class="publishDetails">';
    var closePublishWrapper = '</div>';
    var articleCaptionString = '<h2 class="card-title hidden visually-hidden">No Caption Provided</h2>';
-   var articleAuthorString = '<p class="card-text articleAuthor credits hidden visually-hidden">No Author Provided</p>';
+//    var articleAuthorString = '<p class="card-text articleAuthor credits hidden visually-hidden">No Author Provided</p>';
    var articlePhotoCreditString = '<p class="card-text articlePhotoCredit credits hidden visually-hidden">No Photographer Provided</p>';
    var imageString = '<img class="hidden visually-hidden" />';
    var dateString = '<p class="publishDate card-text"><small>' + publishDate + '</small></p>';
@@ -72,7 +72,7 @@ try {
     * */
    if (articleImage != "") {
        openImageWrapper = '<div class="imageWrapper">';
-       imageString = '<img src="' + articleImage + '" class="articleImage card-img" alt="' + articleImageAlt + '" />';
+       imageString = '<img src="' + articleImage + '" class="articleImage card-img" alt="' + contentName + '" />';
    }
 
 
@@ -93,9 +93,9 @@ try {
     *  Parse for author
     * 
     * */
-   if (articleAuthor != "") {
-       articleAuthorString = '<p class="card-text articleAuthor credits">By ' + articleAuthor + '</p>';
-   }
+//    if (articleAuthor != "") {
+//        articleAuthorString = '<p class="card-text articleAuthor credits">By ' + articleAuthor + '</p>';
+//    }
 
 
 
@@ -123,7 +123,7 @@ try {
    document.write(closeImageWrapper);
    document.write(openBodyWrapper);
    document.write(articleCaptionString);
-   document.write(articleAuthorString);
+//    document.write(articleAuthorString);
    document.write(articlePhotoCreditString);
    document.write(openPublishWrapper);
    document.write(externalLinkString);
