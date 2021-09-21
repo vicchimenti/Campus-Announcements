9a617325b3d6c7779c571de6eb5c28431ada7e11
Vic Chimenti <victor.chimenti@gmail.com>
2021-09-21 08:22:43 -0700

    wrap main in try catch

--- ogranizer-base-dev.js
+++ ogranizer-base-dev.js
@@ -13,7 +13,7 @@
 *
 *     Adapted from the existing organizer organizer.js media library id 163514
 *
-*     @version 2.24
+*     @version 2.25
 */
 
 
@@ -304,190 +304,197 @@ function byCustomElements(cid, elements) {
  */
 function main(header, midder, footer) {
 
-    /**
-     * Set variables
-     */
-    
+    try {
+        /**
+         * Set variables
+         */
+        
 
-    // gets the title
-    var title = (content.hasElement('Title') ? content.get('Title') : "");
-    // users choice for the content type to sort
-    var choice = content.get('Content type and layout').publish();
-    // parses out the content id that references the content type in the media library
-    var CID = new java.lang.Integer(choice.split(";")[0]);
-    // parses layout options from the content type choice
-    var LAYOUT = choice.split(";")[1];
-    // section id
-    var SSID = String(content.get('Section')).match(/sslink_id="(\d+)"/)[1];
-    // the required sort method from the list of options
-    var sortMethod = content.get('Sorting method').publish();
-    // the optional custom elements that a user can sort by - this can be any length of items
-    var sElement = String(content.get('Custom element'));
-    // the reverse order option
-    var bReverse = !content.get('Reverse order').isNull();
-    // the paginate option to display items on multiple pages
-    var bPaginate = (content.hasElement('Paginate?') ? !content.get('Paginate?').isNull() : null);
-    // the number of items to display on each page when pagination is true
-    var nPerPage = (content.hasElement('Total number of items to display per page') ? content.get('Total number of items to display per page') : 0);
-    // the number of items to display
-    var LIMIT = content.get('Total number of items to display');
-    // user has the option of beginning their display at any item rather than the first
-    var nStart = (content.get('Start Number') && content.get('Start Number') > 0 ? content.get('Start Number') : 1);
-    
-    // the logic to determine layouts and links that were available to the user
-    var bViewAll = (content.hasElement('Show link to original section') ? !content.get('Show link to original section').isNull() : false);
-    var sViewAllText = (content.hasElement('Link to original section text') ? content.get('Link to original section text') : "");
-    if (sViewAllText == "")
-        sViewAllText = "View All";
-    var bSummFirst = (LAYOUT == "v9/organizer/newsArticleSummary/Link");
-    if (bSummFirst) {
-        LAYOUT = "v9/organizer/newsArticleSummary";
-    }
-
-
-
-
-    /**
-     * Get section
-     */
-    var oSSLM = ServerSideLinkManager.getManager();
-    var mySectionLinkID = Number(SSID);
-    var myLink = oSSLM.getLink(dbStatement.getConnection(), mySectionLinkID, section.getID(), content.getID(), language);
-    var sectionID = myLink.getToSectionID();
-
-
-
-
-    /**
-     * Get content from section
-     */
-    var oChannel = publishCache.getChannel();
-    var oSection = TreeTraversalUtils.findSection(oChannel, section, sectionID, language);
-    var dSequence = oSection.getContentsAndSequences();
-    var mode = getMode(isPreview);
-    var aSCI = oSection.getContent(oChannel, language, mode);
-    var mirrorContent = [];
-    for (var i = 0; i < aSCI.length; i++) {
-        var item = aSCI[i].getContent();
-        mirrorContent.push(item);
-    }
-
-
-    
-    
-
-    /**
-     * Filter content that matches content type
-     */
-    var oCM = ApplicationContextProvider.getBean(com.terminalfour.content.IContentManager);
-    var validContent = [];
-    for (var i = 0; i < mirrorContent.length; i++) {
-        var item = {
-            Content: oCM.get(mirrorContent[i].ID, language),
-            CachedContent: mirrorContent[i],
-            index: dSequence.get(new java.lang.Integer(mirrorContent[i].ID))
-        };
-        if (item.Content.getContentTypeID() == CID) {
-            validContent.push(item);
+        // gets the title
+        var title = (content.hasElement('Title') ? content.get('Title') : "");
+        // users choice for the content type to sort
+        var choice = content.get('Content type and layout').publish();
+        // parses out the content id that references the content type in the media library
+        var CID = new java.lang.Integer(choice.split(";")[0]);
+        // parses layout options from the content type choice
+        var LAYOUT = choice.split(";")[1];
+        // section id
+        var SSID = String(content.get('Section')).match(/sslink_id="(\d+)"/)[1];
+        // the required sort method from the list of options
+        var sortMethod = content.get('Sorting method').publish();
+        // the optional custom elements that a user can sort by - this can be any length of items
+        var sElement = String(content.get('Custom element'));
+        // the reverse order option
+        var bReverse = !content.get('Reverse order').isNull();
+        // the paginate option to display items on multiple pages
+        var bPaginate = (content.hasElement('Paginate?') ? !content.get('Paginate?').isNull() : null);
+        // the number of items to display on each page when pagination is true
+        var nPerPage = (content.hasElement('Total number of items to display per page') ? content.get('Total number of items to display per page') : 0);
+        // the number of items to display
+        var LIMIT = content.get('Total number of items to display');
+        // user has the option of beginning their display at any item rather than the first
+        var nStart = (content.get('Start Number') && content.get('Start Number') > 0 ? content.get('Start Number') : 1);
+        
+        // the logic to determine layouts and links that were available to the user
+        var bViewAll = (content.hasElement('Show link to original section') ? !content.get('Show link to original section').isNull() : false);
+        var sViewAllText = (content.hasElement('Link to original section text') ? content.get('Link to original section text') : "");
+        if (sViewAllText == "")
+            sViewAllText = "View All";
+        var bSummFirst = (LAYOUT == "v9/organizer/newsArticleSummary/Link");
+        if (bSummFirst) {
+            LAYOUT = "v9/organizer/newsArticleSummary";
         }
-    }
 
 
 
 
-    /**
-     * Sort content
-     */
-    if (sElement != "") {
-        // when the user selects any custom sort element
-        var arrayOfElements = [];
-        arrayOfElements = sElement.split(',');
-        validContent.sort(eval(byCustomElements(CID, arrayOfElements)));
-    } else {
-        // when the user only sorts by the default options
-        validContent.sort(eval(sortMethod + '(' + CID + ', sElement);'));
-    }
-    if (bReverse)
-        validContent.reverse();
+        /**
+         * Get section
+         */
+        var oSSLM = ServerSideLinkManager.getManager();
+        var mySectionLinkID = Number(SSID);
+        var myLink = oSSLM.getLink(dbStatement.getConnection(), mySectionLinkID, section.getID(), content.getID(), language);
+        var sectionID = myLink.getToSectionID();
 
 
 
 
-    /**
-     * Display content
-     */
-    if (!header)
-        header = "";
-    if (!midder)
-        midder = "";
-    if (!footer)
-        footer = "";
-    if (title != "")
-        header = header + '<h2 class="organizerTitle">' + title + '</h2>';
-    if (bViewAll) {
-        var href = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Section" output="linkurl" modifiers="nav_sections" />');
-        midder = midder + '<div class="boxlinkItem viewAll"><a href="' + href + '">' + sViewAllText + '</a></div>';
-    }
-
-
-
-    /**
-     * Determine Pagination
-     */
-    if (bPaginate && !bSummFirst) {
-        // when the user selects a content type with Summary in the Content type and layout option while also selecting Paginate
-
-        var contentInfo = [];
-        for (var i = nStart - 1; i < validContent.length && !isLimitPassed(i, LIMIT); i++) {
-            var tci = new TargetContentInfo(validContent[i].CachedContent, oSection, language);
-            contentInfo.push(tci);
+        /**
+         * Get content from section
+         */
+        var oChannel = publishCache.getChannel();
+        var oSection = TreeTraversalUtils.findSection(oChannel, section, sectionID, language);
+        var dSequence = oSection.getContentsAndSequences();
+        var mode = getMode(isPreview);
+        var aSCI = oSection.getContent(oChannel, language, mode);
+        var mirrorContent = [];
+        for (var i = 0; i < aSCI.length; i++) {
+            var item = aSCI[i].getContent();
+            mirrorContent.push(item);
         }
-        var vector = new java.util.Vector(java.util.Arrays.asList(contentInfo));
-        var sectionPublisher = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.SectionPublisher),
-            contentPublisher = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.ContentPublisher),
-            publishHelper = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.PublishHelper),
-            paginator = new NavigationPaginator(sectionPublisher, contentPublisher, publishHelper);
-        paginator.setContentPerPage((nPerPage > 0 ? nPerPage : 10));
-        paginator.setFormatter(LAYOUT);
-        paginator.setLinksToShow(10);
-        var before = '<div class="paginationWrapper"><div class="pagination"><span class="paginationNumber">';
-        var middle = '</span><span class="paginationNumber">';
-        var after = '</span></div></div>';
-        paginator.setPageSeparators(before, middle, after);
-        paginator.setBeforeAndAfterHTML(header, footer);
-        paginator.setPreview(isPreview);
-        paginator.write(document, dbStatement, publishCache, section, language, isPreview, vector);
 
-        // eventually we may want an else if here EX: else if (bPaginate && bSummFirst) {...}
-        // that would allow when the Summary and Paginate option are both chosen
-        // however at this time I haven't been able to produce a solution that merges
-        // the paginator with the oCP but it should be possible with enough time to work it out
-        // for now we go straight to the else
-        // and we must communicate to our departments that we don't support that functionality
-        // when they try to select both summary and paginator
-        // Victor 7/2020
-    } else {
-        document.write(header);
-        var oSW = new java.io.StringWriter();
-        var oT4SW = new T4StreamWriter(oSW);
-        var oCP = new ContentPublisher();
-        // prepare for first content item
-        first = true;
-        for (var i = nStart - 1; i < validContent.length && !isLimitPassed(i, LIMIT); i++) {
-            // if first print content item completely
-            if (first) {
-                oLayout = LAYOUT;
-                first = false;
-            // if not first print link version if requested but normally otherwise
-            } else {
-                oLayout = bSummFirst ? LAYOUT + "/Link" : LAYOUT;
+
+        
+        
+
+        /**
+         * Filter content that matches content type
+         */
+        var oCM = ApplicationContextProvider.getBean(com.terminalfour.content.IContentManager);
+        var validContent = [];
+        for (var i = 0; i < mirrorContent.length; i++) {
+            var item = {
+                Content: oCM.get(mirrorContent[i].ID, language),
+                CachedContent: mirrorContent[i],
+                index: dSequence.get(new java.lang.Integer(mirrorContent[i].ID))
+            };
+            if (item.Content.getContentTypeID() == CID) {
+                validContent.push(item);
             }
-            oCP.write(oT4SW, dbStatement, publishCache, oSection, validContent[i].Content, oLayout, isPreview);
         }
 
-        document.write(oSW.toString());
-        document.write(midder);
-        document.write(footer);
+
+
+
+        /**
+         * Sort content
+         */
+        if (sElement != "") {
+            // when the user selects any custom sort element
+            var arrayOfElements = [];
+            arrayOfElements = sElement.split(',');
+            validContent.sort(eval(byCustomElements(CID, arrayOfElements)));
+        } else {
+            // when the user only sorts by the default options
+            validContent.sort(eval(sortMethod + '(' + CID + ', sElement);'));
+        }
+        if (bReverse)
+            validContent.reverse();
+
+
+
+
+        /**
+         * Display content
+         */
+        if (!header)
+            header = "";
+        if (!midder)
+            midder = "";
+        if (!footer)
+            footer = "";
+        if (title != "")
+            header = header + '<h2 class="organizerTitle">' + title + '</h2>';
+        if (bViewAll) {
+            var href = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Section" output="linkurl" modifiers="nav_sections" />');
+            midder = midder + '<div class="boxlinkItem viewAll"><a href="' + href + '">' + sViewAllText + '</a></div>';
+        }
+
+
+
+        /**
+         * Determine Pagination
+         */
+        if (bPaginate && !bSummFirst) {
+            // when the user selects a content type with Summary in the Content type and layout option while also selecting Paginate
+
+            var contentInfo = [];
+            for (var i = nStart - 1; i < validContent.length && !isLimitPassed(i, LIMIT); i++) {
+                var tci = new TargetContentInfo(validContent[i].CachedContent, oSection, language);
+                contentInfo.push(tci);
+            }
+            var vector = new java.util.Vector(java.util.Arrays.asList(contentInfo));
+            var sectionPublisher = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.SectionPublisher),
+                contentPublisher = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.ContentPublisher),
+                publishHelper = com.terminalfour.spring.ApplicationContextProvider.getBean(com.terminalfour.publish.PublishHelper),
+                paginator = new NavigationPaginator(sectionPublisher, contentPublisher, publishHelper);
+            paginator.setContentPerPage((nPerPage > 0 ? nPerPage : 10));
+            paginator.setFormatter(LAYOUT);
+            paginator.setLinksToShow(10);
+            var before = '<div class="paginationWrapper"><div class="pagination"><span class="paginationNumber">';
+            var middle = '</span><span class="paginationNumber">';
+            var after = '</span></div></div>';
+            paginator.setPageSeparators(before, middle, after);
+            paginator.setBeforeAndAfterHTML(header, footer);
+            paginator.setPreview(isPreview);
+            paginator.write(document, dbStatement, publishCache, section, language, isPreview, vector);
+
+            // eventually we may want an else if here EX: else if (bPaginate && bSummFirst) {...}
+            // that would allow when the Summary and Paginate option are both chosen
+            // however at this time I haven't been able to produce a solution that merges
+            // the paginator with the oCP but it should be possible with enough time to work it out
+            // for now we go straight to the else
+            // and we must communicate to our departments that we don't support that functionality
+            // when they try to select both summary and paginator
+            // Victor 7/2020
+        } else {
+            document.write(header);
+            var oSW = new java.io.StringWriter();
+            var oT4SW = new T4StreamWriter(oSW);
+            var oCP = new ContentPublisher();
+            // prepare for first content item
+            first = true;
+            for (var i = nStart - 1; i < validContent.length && !isLimitPassed(i, LIMIT); i++) {
+                // if first print content item completely
+                if (first) {
+                    oLayout = LAYOUT;
+                    first = false;
+                // if not first print link version if requested but normally otherwise
+                } else {
+                    oLayout = bSummFirst ? LAYOUT + "/Link" : LAYOUT;
+                }
+                oCP.write(oT4SW, dbStatement, publishCache, oSection, validContent[i].Content, oLayout, isPreview);
+            }
+
+            document.write(oSW.toString());
+            document.write(midder);
+            document.write(footer);
+        }
+        // End Main
+
+
+
+    } catch (e) {
+        log("Error Thrown: " + e);
     }
 }
-
