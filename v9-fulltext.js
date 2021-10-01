1ba7cbed75e061f8a07377dd7cc3c7e5719825e7
Vic Chimenti <victor.chimenti@gmail.com>
2021-10-01 15:43:49 -0700

    rm feed

--- v9-fulltext.js
+++ v9-fulltext.js
@@ -5,7 +5,7 @@
  *
  *     Document will write client side once when the page loads
  *
- *     @version 6.22
+ *     @version 6.23
  */
 
 
@@ -112,7 +112,6 @@ try {
         audience: getContentValues('<t4 type="content" name="Audience" output="normal" display_field="value" />'),
         topics: getContentValues('<t4 type="content" name="Topic" output="normal" display_field="value" />'),
         priority: getContentValues('<t4 type="content" name="Priority" output="normal" display_field="value" />'),
-        layoutFeed: getContentValues('<t4 type="content" name="Feed" output="normal" display_field="value" />'),
         sectionLink: getContentValues('<t4 type="content" name="Section Link 1" output="linkurl" modifiers="nav_sections" />'),
         sectionLinkText: getContentValues('<t4 type="content" name="Section Link" output="linktext" modifiers="nav_sections" />'),
         fullTextLink: getContentValues('<t4 type="content" name="Name" output="fulltext" use-element="true" filename-element="Article Title" modifiers="striptags,htmlentities" />'),
