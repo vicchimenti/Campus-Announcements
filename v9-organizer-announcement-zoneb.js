a91a2df12b064388afe2f0f77dee1b46d1d4748b
Vic Chimenti <victor.chimenti@gmail.com>
2021-10-01 15:43:07 -0700

    udr

--- v9-organizer-announcement-zoneb.js
+++ v9-organizer-announcement-zoneb.js
@@ -1,5 +1,5 @@
   /***
-   *     @author Victor Chimenti, MSCS-SE '20
+   *     @author Victor Chimenti, MSCS
    *     @file v9-organizer-announcementZoneb.js
    *     v9/organizer/announcementZoneb
    *     id:5580
