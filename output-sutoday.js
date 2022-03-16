d553320898083f9b8df626f06152040bd79fd249
vicchimenti <chimenti@seattleu.edu>
2022-03-16 14:04:39 -0700

    trim function

--- output-sutoday.js
+++ output-sutoday.js
@@ -10,6 +10,7 @@
      *     Document will write once when the page loads
      *
      *     @version 7.0
+     * 
      */
 
 
@@ -36,7 +37,7 @@
      */
     function getContentValues(tag) {
         try {
-            var _tag = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, tag)
+            var _tag = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, tag).trim()
             return {
                 isError: false,
                 content: _tag == '' ? null : _tag
